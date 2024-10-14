bedc708bc591808f31e244c9af82d845825f5bec
Wallace Artur <wallacearthur_3@hotmail.com>
2024-10-14 11:37:04 -0300

    delete UserById

--- src/main/java/com/wallacearthur/workshopmongo/services/UserService.java
+++ src/main/java/com/wallacearthur/workshopmongo/services/UserService.java
@@ -25,16 +25,23 @@ public class UserService {
 			
 		public User findById(String id) {
 			Optional<User> obj = userRepository.findById(id);
-			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 		}
 		
 		public User insert(User obj) {
 			return userRepository.insert(obj);
 		}
 		
+		public void delete(String id) {
+			findById(id);
+			userRepository.deleteById(id);
+		}
+		
 		public User fromDTO(UserDTO objDto) {
 			return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 		}
+		
+		
 	
 	}
 
