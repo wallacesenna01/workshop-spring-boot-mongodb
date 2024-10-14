2a953afbccd0d6fd377bb41bfda26cd631822a82
Wallace Artur <wallacearthur_3@hotmail.com>
2024-10-14 10:49:15 -0300

    getting UserById

--- src/main/java/com/wallacearthur/workshopmongo/services/UserService.java
+++ src/main/java/com/wallacearthur/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.wallacearthur.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wallacearthur.workshopmongo.domain.User;
 import com.wallacearthur.workshopmongo.repository.UserRepository;
+import com.wallacearthur.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,13 @@ public class UserService {
 	public List<User> findAll(){
 		
 		return userRepository.findAll();
+		
 	}
-}
+			
+		public User findById(String id) {
+			Optional<User> obj = userRepository.findById(id);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+	}
+
