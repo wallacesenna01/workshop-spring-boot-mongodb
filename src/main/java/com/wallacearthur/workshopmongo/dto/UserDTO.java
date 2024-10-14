a498956b7918c4657356e7fdd57ea6859d597157
Wallace Artur <wallacearthur_3@hotmail.com>
2024-10-14 11:24:03 -0300

    Insert User with Post

--- src/main/java/com/wallacearthur/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/wallacearthur/workshopmongo/dto/UserDTO.java
@@ -46,4 +46,6 @@ public class UserDTO implements Serializable{
 	}
 	
 	
+	
+	
 }
--- src/main/java/com/wallacearthur/workshopmongo/services/UserService.java
+++ src/main/java/com/wallacearthur/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wallacearthur.workshopmongo.domain.User;
+import com.wallacearthur.workshopmongo.dto.UserDTO;
 import com.wallacearthur.workshopmongo.repository.UserRepository;
 import com.wallacearthur.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,6 +26,14 @@ public class UserService {
 		public User findById(String id) {
 			Optional<User> obj = userRepository.findById(id);
 			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+		
+		public User insert(User obj) {
+			return userRepository.insert(obj);
+		}
+		
+		public User fromDTO(UserDTO objDto) {
+			return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 		}
 	
 	}
