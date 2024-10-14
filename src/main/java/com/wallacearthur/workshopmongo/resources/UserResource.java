d47b9941df716db0fc1754423218669b18913045
Wallace Artur <wallacearthur_3@hotmail.com>
2024-10-14 09:53:36 -0300

    Connect mongoDB with Repository and Service

--- src/main/java/com/wallacearthur/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wallacearthur/workshopmongo/resources/UserResource.java
@@ -4,26 +4,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wallacearthur.workshopmongo.domain.User;
+import com.wallacearthur.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
-	@GetMapping()
-	public ResponseEntity<List<User>> findAll(){		
-		User maria = new User("1", "Maria Silva", "maria@gmail.com");
-		User alex = new User("2", "Alex Green", "alex@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria,alex));
-		return ResponseEntity.ok().body(list);
 	
-		
+	@Autowired
+	private UserService service;
+	
+	@GetMapping()
+	public ResponseEntity<List<User>> findAll(){				
+		List<User> list = service.findAll();	
+		return ResponseEntity.ok().body(list);	
 	}
 
 }
