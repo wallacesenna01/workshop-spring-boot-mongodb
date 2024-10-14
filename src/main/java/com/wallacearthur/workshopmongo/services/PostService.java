29b9f18172ee4e7672778b87315b056b3e9410c5
Wallace Artur <wallacearthur_3@hotmail.com>
2024-10-14 14:16:32 -0300

    Getting postById

---
+++ src/main/java/com/wallacearthur/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.wallacearthur.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wallacearthur.workshopmongo.domain.Post;
+import com.wallacearthur.workshopmongo.repository.PostRepository;
+import com.wallacearthur.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+
+			
+		public Post findById(String id) {
+			Optional<Post> obj = postRepository.findById(id);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+		}
+		
+	
+	}
+
